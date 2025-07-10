49b45a9aff2b467dd6646442ff488fcee6cd3e08
unknown <oliver.heckel2002@gmail.com>
2025-07-10 18:12:50 +0200

    Deploy portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const projects = [
       'This is a team project developed during the course "Teamprojekt" ( Winter Semester 2024/25 ) for the [@brendel-group](https://github.com/brendel-group). \n mission_db is a web-based application to work with and organize Polybots missions. \n I was primarily responsible for implementing the frontend API integration using TypeScript (fetch API), and also contributed to the user interface and other aspects of the application. \n This project was a collaborative effort with a team of 6 students from the Eberhard Karls Universität Tübingen, Germany. \n Team members: [@Felix Mycka](https://github.com/sh1negg), [@Pascal Kuppler](https://github.com/pascal260303), [@Samuel Hempelmann](https://github.com/SamuABC), [@Simon Rappenecker](https://github.com/DerSimi), [@Simon Rappold](https://github.com/s-rappold).',
     stack: ['Python', 'TypeScript', 'Django', 'Rest API', 'PostgreSQL'],
     sourceCode: 'https://github.com/brendel-roup/mission_db',
-    //livePreview: 'https://github.com',
+    livePreview: '',
   },
 ]
 
