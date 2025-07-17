278f3e468ae66b7ec354e88a47b72d2edf2f16cb
unknown <oliver.heckel2002@gmail.com>
2025-07-17 19:59:57 +0200

    Change URL in header to oliverheckel.com

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://KageKrito.github.io',
+  homepage: 'https://oliverheckel.com',
   title: 'Oliver Heckel',
 }
 
