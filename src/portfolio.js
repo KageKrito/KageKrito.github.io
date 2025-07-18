202b9d5f0500bfd8ee7fbf7d7d4fb949e8e06a9d
unknown <oliver.heckel2002@gmail.com>
2025-07-18 16:40:53 +0200

    Change name on header to initials

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://oliverheckel.com',
-  title: 'Oliver Heckel',
+  title: 'OH.',
 }
 
 const about = {
